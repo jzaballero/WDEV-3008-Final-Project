7134967d8c36bde3af0f71c048d85ffc46481bfb
Jewell Zaballero <jzaballero@academic.rrc.ca>
2021-11-15 19:56:15 -0600

    Initialized User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_015106) do
+ActiveRecord::Schema.define(version: 2021_11_16_015349) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,20 @@ ActiveRecord::Schema.define(version: 2021_11_16_015106) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.integer "province_id", null: false
+    t.string "password"
+    t.string "address"
+    t.string "city"
+    t.string "postal_code"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["province_id"], name: "index_users_on_province_id"
+  end
+
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
+  add_foreign_key "users", "provinces"
 end

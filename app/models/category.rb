8c867be8d16ddc83fe83f2428ce7abfeabc31693
Jewell Zaballero <jzaballero@academic.rrc.ca>
2021-11-18 12:07:15 -0600

    Fixed category price range validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,5 @@ class Category < ApplicationRecord
   has_many :products, through: :product_categories
   has_many :product_categories
 
-  validates :name, presence: true
-  validates :price_range, presence: true, numericality: true
+  validates :name, :price_range, presence: true
 end

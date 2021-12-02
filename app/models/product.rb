80c44d3c697b325f283a280f10aeed0f00d81b08
Jewell Zaballero <jzaballero@academic.rrc.ca>
2021-12-02 16:42:51 -0600

    Can edit product categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,5 +10,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
   validates :price, :pokemon_number, numericality: true
-  validates :image, :pokemon_name, :pokemon_type, length: { minimum: 2, maximum: 128 }
+  validates :pokemon_name, :pokemon_type, length: { minimum: 2, maximum: 128 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_182724) do
+ActiveRecord::Schema.define(version: 2021_12_02_205425) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -118,7 +118,6 @@ ActiveRecord::Schema.define(version: 2021_11_24_182724) do
     t.string "name"
     t.text "description"
     t.decimal "price"
-    t.string "image"
     t.string "pokemon_name"
     t.string "pokemon_type"
     t.integer "pokemon_number"
